33409ba42b3c924c13c59010fa40c1e078c914a5
M1kuuuu <131905590+M1kuuuu@users.noreply.github.com>
2026-03-26 01:44:43 +0200

    Update Queue.h

--- DynamicDataCollections/DynamicDataCollections/Queue.h
+++ DynamicDataCollections/DynamicDataCollections/Queue.h
@@ -1,5 +1,107 @@
 #pragma once
+#include <iostream>
 class Queue
 {
+public:
+	struct Node
+	{
+		int Value;
+		Node* Next;
+	};
+
+private:
+	Node* BeginList;
+
+public:
+	Queue()
+	{
+		BeginList = nullptr;
+	}
+	~Queue()
+	{
+		Clear();
+	}
+
+	void Add(int value)
+	{
+		Node* newNode = new Node();
+		Node* current = BeginList;
+		newNode->Value = value;
+		newNode->Next = nullptr;
+
+		if (BeginList == nullptr)
+		{
+			BeginList = newNode;
+			return;
+		}
+
+		while (current->Next != nullptr)
+		{
+			current = current->Next;
+		}
+
+		current->Next = newNode;
+	}
+	void Clear()
+	{
+		if (BeginList == nullptr)
+			return;
+
+		Node* current = BeginList;
+		Node* next = nullptr;
+
+		while (current != nullptr)
+		{
+			next = current->Next;
+			delete current;
+			current = next;
+		}
+		BeginList = nullptr;
+	}
+	void PrintToConsole()
+	{
+		if (BeginList == nullptr)
+		{
+			std::cout << "AJle enTa. oH nycT" << std::endl;
+			return;
+		}
+
+		Node* current = BeginList;
+		Node* next = nullptr;
+
+		std::cout << "JlaH, BblBo2ly cnucok" << std::endl;
+		while (current != nullptr)
+		{
+			std::cout << "Tekylljee zHa4eHue -- " << current->Value << "\n";
+			current = current->Next;
+
+		}
+		std::cout << std::endl;
+
+	}
+	void Remove()
+	{
+		if (BeginList == nullptr)
+			return;
+
+		Node* current = BeginList;
+		BeginList = BeginList->Next;
+		delete current;
+	}
+
+	bool IsContain(int value)
+	{
+		Node* current = BeginList;
+		while (current != nullptr)
+		{
+			if (current->Value == value)
+				return true;
+			current = current->Next; 
+		}
+		return false;
+	}
+
+private:
 };
+
 
